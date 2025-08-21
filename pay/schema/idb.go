c056fbfeceae2d1c2344f877f2ae6ad2922568da
hymatrix-core <biz@hymatrix.com>
2025-08-21 14:12:16 +0800

    build pay cache

--- pay/schema/idb.go
+++ pay/schema/idb.go
@@ -3,28 +3,24 @@ package schema
 import "math/big"
 
 type IDB interface {
-	//////////////////////
-	// read only
 	IsWhitelist(accid string) bool
+	SetWhitelist(accid string, enabled bool) error
+
 	IsExecuted(txHash string) bool
+	MarkExecuted(txHash string) error
 
 	SponsorTotal(sponsor string) *big.Int
 	SponsorBreakdown(sponsor string) map[string]*big.Int
-
 	BeneficiaryTotal(beneficiary string) *big.Int
 	BeneficiaryBreakdown(beneficiary string) map[string]*big.Int
-
-	//////////////////////
-	// write
-	SetWhitelist(accid string, enabled bool) error
-	MarkExecuted(txHash string) error
 
 	Deposit(sponsor, beneficiary string, amount *big.Int) error
 	Withdraw(sponsor, beneficiary string, amount *big.Int) error
 
+	CanCover(beneficiary string, delta *big.Int) bool
 	UseOnce(beneficiary, pid string, fee *big.Int) error
-	SpawnFee(beneficiary string, fee *big.Int) error
-	ResidencyFee(beneficiary string, fee *big.Int) error
+	SpawnFee(beneficiary, pid string, fee *big.Int) error
+	ResidencyFee(pid string, fee *big.Int) error
 	// devFees pid -> fee
 	SettleTxFee(beneficiary string, devShareRatio *big.Int) (nodeFee *big.Int, devFees map[string]*big.Int, err error)
 
