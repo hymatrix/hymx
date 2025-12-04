795e1ce09de46baa6f9ce95baacfb3dea85844e6
Potter <45678072+cryptowizard0@users.noreply.github.com>
2025-12-04 11:48:06 +0800

    Refactor module commands and functions
    Split the 'module' command into 'module_gen', 'module_load', and 'module_upload' in main.go for better clarity and functionality. Added corresponding functions in module.go to generate, upload, and load modules, improving modularity and error handling.

--- examples/module.go
+++ examples/module.go
@@ -1,29 +1,33 @@
 package main
 
 import (
-	"encoding/json"
 	"fmt"
-	"os"
 
 	"github.com/hymatrix/hymx/schema"
 	vmmSchema "github.com/hymatrix/hymx/vmm/schema"
 )
 
-func module() {
-	// item, _ := s.GenerateModule([]byte{}, schema.Module{
-	// 	Base:         schema.DefaultBaseModule,
-	// 	ModuleFormat: "org.type.1.0.0",
-	// })
-	item, _ := s.GenerateModule([]byte{}, schema.Module{
+func genModule() {
+	itemId, err := s.GenerateAndSaveModule([]byte{}, schema.Module{
 		Base:         schema.DefaultBaseModule,
 		ModuleFormat: vmmSchema.ModuleFormatRegistry,
 	})
-	// item, _ := s.GenerateModule([]byte{}, schema.Module{
-	// 	Base:         schema.DefaultBaseModule,
-	// 	ModuleFormat: vmmSchema.ModuleFormatToken,
-	// })
-	bin, _ := json.Marshal(item)
+	if err != nil {
+		fmt.Println("generate and save module failed, ", "err", err)
+		return
+	}
+	fmt.Println("generate and save module success, ", "id", itemId)
+}
 
-	filename := fmt.Sprintf("mod-%s.json", item.Id)
-	os.WriteFile(filename, bin, 0644)
+func uploadModule(modFile, keyFile string) (txid string, err error) {
+	return s.UploadModuleToArweave(modFile, keyFile)
+}
+
+func loadModule(itemId string) {
+	module, err := s.Client.GetModule(itemId)
+	if err != nil {
+		fmt.Println("load module failed, ", "id", itemId, "err", err)
+		return
+	}
+	fmt.Printf("load module success, id: %s, module: %+v\n", itemId, module)
 }
